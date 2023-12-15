7302e2789682ba67c2fd3db84b23ffaf375bc217
Åke Rosvall <vrikenz@gmail.com>
2023-12-15 08:48:34 +0000

    day2 part2

--- day2.py
+++ day2.py
@@ -22,3 +22,27 @@ sum_of_possible_game_ids
 #2632
 
 #part 2
+def minimum_cubes_per_game(game_record):
+    max_red, max_green, max_blue = 0, 0, 0
+    subsets = game_record.strip().split('; ')
+    for subset in subsets:
+        red_count = sum(int(num.split()[0]) for num in subset.split(',') if 'red' in num)
+        green_count = sum(int(num.split()[0]) for num in subset.split(',') if 'green' in num)
+        blue_count = sum(int(num.split()[0]) for num in subset.split(',') if 'blue' in num)
+        
+        max_red = max(max_red, red_count)
+        max_green = max(max_green, green_count)
+        max_blue = max(max_blue, blue_count)
+
+    return max_red, max_green, max_blue
+
+total_power = 0
+for game in game_data:
+    _, game_record = game.split(': ')
+    min_red, min_green, min_blue = minimum_cubes_per_game(game_record)
+    game_power = min_red * min_green * min_blue
+    total_power += game_power
+
+total_power
+
+#69629
