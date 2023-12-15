97b113279ba43618e562829a0743be14222d72dc
Åke Rosvall <vrikenz@gmail.com>
2023-12-15 09:20:16 +0100

    part 2

--- day1.py
+++ day1.py
@@ -1,7 +1,3 @@
-#data
-with open(file_path, 'r') as file:
-    lines = file.readlines()
-
 #part 1
 def extract_and_sum_calibrations(lines):
     total_sum = 0
@@ -17,3 +13,33 @@ calibration_sum = extract_and_sum_calibrations(lines)
 calibration_sum
 
 #53386
+
+#part 2
+num_map = {
+    'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
+    'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+}
+
+def word_to_digit(word):
+    return num_map.get(word)
+
+def find_numbers(line):
+    words = line.split()
+    found_nums = []
+    for word in words:
+        if word.isdigit():
+            found_nums.append(word)
+        elif word_to_digit(word):
+            found_nums.append(word_to_digit(word))
+    return found_nums
+
+total_sum = 0
+for line in lines:
+    numbers = find_numbers(line.strip())
+    if numbers:
+        total_sum += int(numbers[0]) + int(numbers[-1])
+
+print(total_sum)
+
+
+#53312
